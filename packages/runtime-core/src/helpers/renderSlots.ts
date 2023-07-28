222130469be9e80751639b0a6a215ac217dc72d1
Pluto <1830233276@qq.com>
2023-07-28 10:49:32 +0800

    update2

--- packages/runtime-core/src/helpers/renderSlots.ts
+++ packages/runtime-core/src/helpers/renderSlots.ts
@@ -5,7 +5,7 @@ export function renderSlots(slots, name = 'default', props) {
     const slot = slots[name]
 
     if (slot) {
-        // 存储slots的数据结构是数组 需要手动的将它们转换为虚拟节点
+        // 存储slots的数据结构是普通数组 需要手动的将它们转换为虚拟节点
         if (typeof slot === 'function') {
             return createVNode( Fragment, { name }, slot(props))
         }
